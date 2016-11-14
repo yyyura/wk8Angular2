df413abd9f98715a12abd58c6b89d78bf94f804c
yyyura <yyyura@gmail.com>
2016-11-14 22:48:58 +0000

    final converter

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConverterComponent implements OnInit {
 
+  km: number = 100;
+  miles: number = 60;
+
   constructor() { }
+
+  KmToMiles(){
+    this.miles=this.km*0.621371;
+  }
+
+  MilesToKm(){
+    this.km=this.miles*1.60934;
+  }
 
   ngOnInit() {
   }
